d82368044aa9e299dcf933f7f3dce93ae60f9a58
KING_SRK <srkhn0211@gmail.com>
2025-07-19 11:29:18 +0530

    Initial commit

--- js/script.js
+++ js/script.js
@@ -4,15 +4,17 @@ const navLinks = document.getElementById("navLinks");
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('visible');
-    }
-  });
-}, {
-  threshold: 0.1
-});
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      }
+    });
+  },
+  {
+    threshold: 0.1,
+  }
+);
 
-animatedItems.forEach(el => observer.observe(el));
-
+animatedItems.forEach((el) => observer.observe(el));
